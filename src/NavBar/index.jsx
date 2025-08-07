b80608053fd424fa59d4a4dece155048e5de592b
MateusGesualdo <57274184+MateusGesualdo@users.noreply.github.com>
2025-08-06 21:25:27 -0300

    adicionei funcao para virar cartao e corrigi as funcoes de navegar entre cartoes

--- src/NavBar/index.jsx
+++ src/NavBar/index.jsx
@@ -1,18 +1,22 @@
-export function NavBar(props){
+export function NavBar(props) {
 
     function goToPrevious() {
-        if(props.currentIndex === 0) return
-       props.setCurrentIndex(props.currentIndex -1)
+        if (props.currentIndex === 0) return
+        props.setCurrentIndex(props.currentIndex - 1)
     }
 
     function goToNext() {
-        if(props.currentIndex === props.lastIndex) return
-       props.setCurrentIndex(props.currentIndex +1)
+        if (props.currentIndex === props.lastIndex) return
+        props.setCurrentIndex(props.currentIndex + 1)
+    }
+
+    function flipCard(){
+        props.setAnswerIsVisible(!props.answerIsVisible)
     }
 
     return <div>
         <button onClick={goToPrevious}>anterior</button>
-        <button>mostrar resposta</button>
+        <button onClick={flipCard}>mostrar {props.answerIsVisible ? 'pergunta' : 'resposta'}</button>
         <button onClick={goToNext}>próxima</button>
     </div>
 }
